367a66b8bf4681fb328a42bb52184756b63ef39b
Denis <BorodiyDenisPL@gmail.com>
2025-03-01 23:50:02 +0100

    new update/01.03.2025

---
+++ findall.py
@@ -0,0 +1,31 @@
+import re
+print(re.findall(r"\d", "h32rb17"))
+
+
+
+
+import re
+print(re.findall(r"\w", "h32rb17"))
+
+
+
+import re
+print(re.findall(r"\d+", "h32rb17"))
+
+
+
+import re
+print(re.findall(r"\d*", "h32rb17"))
+
+
+
+import re
+print(re.findall(r"\d{2}", "h32rb17"))
+
+
+
+
+import re
+print(re.findall(r"\d{1,3}", "h32rb17 k825t0m c2994eh"))
+
+
